f24136865ebcb8e09484018563723048da42ba96
Luis Manuel Ramirez Vargas <luis_manuel.ramirez_vargas@syngenta.com>
2021-08-25 23:07:07 -0500

    starter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css'
+import { useState } from 'react'
+import { Counter } from './components/Counter'
 
-function App() {
-  return <div>App</div>
+const App = () => {
+  const [showComponent, setShowComponent] = useState(false)
+
+  const toggleCounter = () => {
+    setShowComponent((prevState) => !prevState)
+  }
+
+  return (
+    <div className="container">
+      <br />
+      {showComponent && <Counter />}
+      <br />
+      <button className="btn btn-primary" onClick={toggleCounter}>
+        Toggle Counter
+      </button>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Counter.js
@@ -0,0 +1,47 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export function Counter() {
+  const [counter1, setCounter1] = useState(0)
+  const [counter2, setCounter2] = useState(0)
+  const [counter1DidChange, setCounter1DidChange] = useState(false)
+
+  const increaseCounter1 = () => {
+    setCounter1((prevState) => prevState + 1)
+  }
+  const increaseCounter2 = () => {
+    setCounter2((prevState) => prevState + 1)
+  }
+
+  const doSomething = () => {
+    if (counter1 >= 10) {
+      setCounter1DidChange(true)
+    }
+  }
+
+  const doSomethingCallback = useCallback(doSomething, [counter1])
+
+  useEffect(() => {
+    doSomethingCallback()
+  }, [doSomethingCallback])
+
+  return (
+    <div>
+      <h1>{counter1}</h1>
+      <button
+        type="button"
+        className={`btn ${counter1DidChange ? 'btn-danger' : 'btn-primary'} `}
+        onClick={increaseCounter1}
+      >
+        Increase Counter #1
+      </button>
+      <h1>{counter2}</h1>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={increaseCounter2}
+      >
+        Increase Counter #2
+      </button>
+    </div>
+  )
+}
