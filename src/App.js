56f5fbb858d6e0e8e6575deffe4c7de46eed5388
Luis Manuel Ramirez Vargas <luis_manuel.ramirez_vargas@syngenta.com>
2021-08-25 20:58:04 -0500

    Starter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css'
+import { SimpleForm } from './components/SimpleForm'
 
-function App() {
-  return <div>App</div>
+const App = () => {
+  return (
+    <div className="container">
+      <br />
+      <div className="card">
+        <div className="card-header">Form</div>
+        <div className="card-body">
+          <SimpleForm />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/SimpleForm.js
@@ -0,0 +1,25 @@
+export const SimpleForm = () => {
+  const submitHandler = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="email">
+          Email
+        </label>
+        <input className="form-control" id="email" type="email" />
+      </div>
+      <div className="mb-3">
+        <label className="form-label" htmlFor="pass">
+          Password
+        </label>
+        <input className="form-control" id="pass" type="password" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Login
+      </button>
+    </form>
+  )
+}
