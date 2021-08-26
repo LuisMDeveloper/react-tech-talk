ff21852784f94c8e1fd3ced07472766b75d28657
Luis Manuel Ramirez Vargas <luis_manuel.ramirez_vargas@syngenta.com>
2021-08-25 23:07:58 -0500

    component did mount

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export function Counter() {
   const [counter1, setCounter1] = useState(0)
@@ -6,6 +6,10 @@ export function Counter() {
   const increaseCounter1 = () => {
     setCounter1((prevState) => prevState + 1)
   }
+
+  useEffect(() => {
+    console.log('component did mount')
+  }, [])
 
   return (
     <div>
